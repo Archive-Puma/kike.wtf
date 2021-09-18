19b7162dcee1899df5972f8c733d84e87ea157d0
Pumita <hola@kike.wtf>
2021-09-19 01:01:35 +0200

    :tada: jQuery methods

--- public/js/computer.js
+++ public/js/computer.js
@@ -1,21 +1,19 @@
 'use strict';
-window.onload = function() {
+window.onload = () => {
     // Drag 'n drop
-    var desktop = document.getElementById('desktop');
-    $('.desktop__icon').each(function(index) {
-        this.style.top = `${10 * index + 10}px`;
-        this.style.left = '10px';
-        $(this).draggable({ containment: desktop });
+    let desktop = $('#desktop');
+    $('.desktop__icon').each((index, element) => {
+        $(element).css('top', `${10 * index + 10}px`);
+        $(element).css('left', '10px');
+        $(element).draggable({ containment: desktop });
     });
-    $('.desktop__window').draggable({ containment: desktop });
-
 
     // Time & Date
-    var pad2 = (n) => (n < 10 ? '0' : '') + n;
-    function setTime() {
-        var time = new Date();
-        document.getElementById('hour').innerText = `${pad2(time.getHours())}:${pad2(time.getMinutes())}`;
-        document.getElementById('date').innerText = `${pad2(time.getDate())}/${pad2(time.getMonth()+1)}/${time.getFullYear()}`;
+    let pad2 = (n) => (n < 10 ? '0' : '') + n;
+    let setTime = () => {
+        let time = new Date();
+        $('#hour').text(`${pad2(time.getHours())}:${pad2(time.getMinutes())}`);
+        $('#date').text(`${pad2(time.getDate())}/${pad2(time.getMonth()+1)}/${time.getFullYear()}`);
     }
     setInterval(setTime, 60000);
     setTime();
